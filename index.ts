2fbc4cf112e846481ef179fa5f343567c98fefec
Zunaira_Khaliq <160762086+Zunaira-Khaliq@users.noreply.github.com>
2024-03-31 22:28:00 +0500

    created currency convertor

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+type CurrencyCode = keyof typeof currency_rates;
+
+const currency_rates = {
+    PKR: 0.0029,
+    USD: 1,
+    EUR: 0.84,
+    GBP: 0.72,
+    TL: 9.25,
+};
+
+export default async function Home() {
+    const convertCurrency = async () => {
+        let answers = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter amount to convert:"
+            },
+            {
+                name: "myCurrency",
+                type: "list",
+                message: "Choose the source currency:",
+                choices: Object.keys(currency_rates)
+            },
+            {
+                name: "conCurrency",
+                type: "list",
+                message: "Choose the target currency:",
+                choices: Object.keys(currency_rates)
+            },
+        ]);
+
+        const { amount, myCurrency, conCurrency } = answers;
+        const convertedAmount = parseFloat(amount) * currency_rates[myCurrency as CurrencyCode] / currency_rates[conCurrency as CurrencyCode];
+        console.log(`${amount} ${myCurrency} is equal to ${convertedAmount.toFixed(2)} ${conCurrency}`);
+        return `${amount} ${myCurrency} is equal to ${convertedAmount.toFixed(2)} ${conCurrency}`;
+    };
+    const result = await convertCurrency();
+    return result;
+};
+console.log(Home());
